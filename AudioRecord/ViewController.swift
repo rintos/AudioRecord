eba217624dbc2f0e1148c12ae79ffbc05dda20ae
VictorSantoryu <victor.santoryu@github.com>
2018-11-07 22:52:09 -0200

    Record audio and play

--- AudioRecord/ViewController.swift
+++ AudioRecord/ViewController.swift
@@ -7,19 +7,98 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
+    @IBOutlet weak var botaoGravar: UIButton!
+    @IBOutlet weak var botaoPlay: UIButton!
+    
+    var soundRecorder : AVAudioRecorder!
+    var soundPlayer : AVAudioPlayer!
+    
+    var fileName : String = "audioFile.m4a"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupRecorder()
+        botaoPlay.isEnabled = false
+        
+    }
+    
+    func getDocumentsDirector() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //iniciando a gravacao
+    func setupRecorder(){
+        let audioFileName = getDocumentsDirector().appendingPathComponent(fileName)
+        let recordSetting = [ AVFormatIDKey : kAudioFormatAppleLossless,
+                              AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue,
+                              AVEncoderBitRateKey : 320000,
+                              AVNumberOfChannelsKey: 2,
+                              AVSampleRateKey : 44100.2 ] as [ String : Any]
+        do {
+            soundRecorder = try AVAudioRecorder(url: audioFileName, settings: recordSetting)
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+        } catch {
+            print(error)
+        }
     }
-
+    
+    //play no audio
+    func setupPlayer(){
+        let audioFileName = getDocumentsDirector().appendingPathComponent(fileName)
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: audioFileName)
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.volume = 1.0
+        } catch {
+            print(error)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        botaoPlay.isEnabled = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        botaoGravar.isEnabled = true
+        botaoPlay.setTitle("Play", for: .normal)
+        
+    }
+    
+    @IBAction func gravarAction(_ sender: Any) {
+        if botaoGravar.titleLabel?.text == "Record" {
+            soundRecorder.record()
+            botaoGravar.setTitle("Stop", for: .normal)
+            botaoPlay.isEnabled = false
+        } else {
+            soundRecorder.stop()
+            botaoGravar.setTitle("Record", for: .normal)
+            botaoPlay.isEnabled = false
+        }
+    }
+    
+    @IBAction func playAction(_ sender: Any) {
+        if botaoPlay.titleLabel?.text == "Play" {
+            botaoPlay.setTitle("Stop", for: .normal)
+            botaoGravar.isEnabled = false
+            setupPlayer()
+            soundPlayer.play()
+        } else {
+            soundPlayer.stop()
+            botaoPlay.setTitle("Play", for: .normal)
+            botaoGravar.isEnabled = false
+        }
+        
+        
+    }
+    
 
 }
 
